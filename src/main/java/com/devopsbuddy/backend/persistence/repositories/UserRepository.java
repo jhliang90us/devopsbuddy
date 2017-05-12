8b9878b7e82dd74971b0e18e32832edf074e5c64
Joseph Liang <jhliang90us@yahoo.com>
2017-05-12 06:19:13 -0400

    Added unique indexes

---
+++ src/main/java/com/devopsbuddy/backend/persistence/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package com.devopsbuddy.backend.persistence.repositories;
+
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by tedonema on 29/03/2016.
+ */
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    /**
+     * Returns a User given a username or null if not found.
+     * @param username The username
+     * @return a User given a username or null if not found.
+     */
+    public User findByUsername(String username);
+
+}
---
+++ src/main/java/com/devopsbuddy/backend/service/UserService.java
@@ -0,0 +1,61 @@
+package com.devopsbuddy.backend.service;
+
+import com.devopsbuddy.backend.persistence.domain.backend.Plan;
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+import com.devopsbuddy.backend.persistence.domain.backend.UserRole;
+import com.devopsbuddy.backend.persistence.repositories.PlanRepository;
+import com.devopsbuddy.backend.persistence.repositories.RoleRepository;
+import com.devopsbuddy.backend.persistence.repositories.UserRepository;
+import com.devopsbuddy.enums.PlansEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+/**
+ * Created by tedonema on 30/03/2016.
+ */
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PlanRepository planRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User createUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles) {
+
+        String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
+
+        Plan plan = new Plan(plansEnum);
+        // It makes sure the plans exist in the database
+        if (!planRepository.exists(plansEnum.getId())) {
+            plan = planRepository.save(plan);
+        }
+
+        user.setPlan(plan);
+
+        for (UserRole ur : userRoles) {
+            roleRepository.save(ur.getRole());
+        }
+
+        user.getUserRoles().addAll(userRoles);
+
+        user = userRepository.save(user);
+
+        return user;
+
+    }
+}
