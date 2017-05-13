761ecbe4784e7e4747c9ac7528bfb768bc9a3089
Joseph Liang <jhliang90us@yahoo.com>
2017-05-13 08:25:06 -0400

    Completed the Forgot My Password Journey implementation

--- src/main/java/com/devopsbuddy/backend/persistence/repositories/UserRepository.java
+++ src/main/java/com/devopsbuddy/backend/persistence/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package com.devopsbuddy.backend.persistence.repositories;
 
 import com.devopsbuddy.backend.persistence.domain.backend.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by tedonema on 29/03/2016.
@@ -16,5 +20,16 @@ public interface UserRepository extends CrudRepository<User, Long> {
      * @return a User given a username or null if not found.
      */
     public User findByUsername(String username);
+    
+    /**
+     * Returns a User for the given email or null if none was found.
+     * @param email The user's email
+     * @return a User for the given email or null if none was found.
+     */
+    User findByEmail(String email);
+
+    @Modifying
+    @Query("update User u set u.password = :password where u.id = :userId")
+    void updateUserPassword(@Param("userId") long userId, @Param("password") String password);
 
 }
